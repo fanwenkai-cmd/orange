ca76b6c030aada2ca171283705eb657e349dd71b
WenKaiFan <1992877323@qq.com>
2021-01-24 16:50:00 +0800

    访问Config Server集群

--- src/main/java/com/qianfeng/configclient/controller/DemoController.java
+++ src/main/java/com/qianfeng/configclient/controller/DemoController.java
@@ -1,17 +1,22 @@
 package com.qianfeng.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableEurekaClient
 public class DemoController {
     @Value("${foo}")
     private String foo;
-    @Value("${server.port}")
-    private String serverPort;
+//    @Value("${server.port}")
+//    private String serverPort;
+    @Value("${user.name}")
+    private String username;
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hello,foo = " + foo + ",server.port = " + serverPort;
+        return "Hello"+username+",foo = " +foo ;
     }
 }
